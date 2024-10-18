972f984ca8b636f70aaf89500be1411e74199c9d
Judi19 <167385372+Judi19@users.noreply.github.com>
2024-10-18 16:42:42 -0600

    Tarea Programacion

---
+++ SuperHeroe.cs
@@ -0,0 +1,35 @@
+﻿using Tarea1;
+
+public class SuperPoder 
+{
+	public SuperPoder()
+	{
+		public string Nombre { get; set; }
+
+	public string IdentidadSecreta { get; set; }
+
+   public string Ciudad { get; set; }
+
+	public bool PuedeVolar { get; set; }
+
+	SuperPoder superPoder;
+
+	public superPoder()
+	{
+		superPoder = new SuperPoder();
+	}
+
+
+	public void Imprimir()
+	{
+		Console.WriteLine($"Nombre:   {Nombre}")
+			Console.WriteLine($"IdentidadSecreta:    {IdentidadSecreta}")
+			Console.WriteLine($"Ciudad:    {Ciudad}")
+			Console.WriteLine($"PuedeVolar:     {PuedeVolar}")
+			Console.WriteLine($"SuperPoder:    {superPoder.Nobre}")
+			Console.WriteLine($"Descripcion:   {superPoder.Descripcion}")
+			Console.WriteLine($"Nivel:     {superPoder.Nivel}")
+			Console.WriteLine(); 
+	}
+}
+
